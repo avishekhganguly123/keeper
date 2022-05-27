0a535b2b4aaa4fb0939de70b907465c48fc76906
Avishekh Ganguly <abishekganguly123@gmail.com>
2022-05-27 23:48:25 +0530

    first commit

---
+++ build/precache-manifest.ab98b050b48a929cd83391133feb780a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e35d52fd88fc0578111ad93807d9ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa1195ee5dceb2a2e56",
+    "url": "/static/js/2.6e44967c.chunk.js"
+  },
+  {
+    "revision": "9e2d59c0fdded4272e5a",
+    "url": "/static/js/main.7c46c200.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
